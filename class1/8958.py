d3d4d733ce77d4fa8d2262e4c062cbdfd933743f
Journey99 <g48993818@gmail.com>
2021-04-24 22:35:38 +0900

    14th day

---
+++ class1/8958.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for i in range(n):
+    total = 0
+    score = 0
+    for j in input():
+        if j == "O":
+            score += 1
+            total += score
+        else:
+            score = 0
+    print(total)
