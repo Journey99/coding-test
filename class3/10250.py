c6e8f6649585f12c1cbf4fdceaed68c2a27a272f
Journey99 <g48993818@gmail.com>
2021-06-09 20:11:00 +0900

    60th day

---
+++ class3/10250.py
@@ -0,0 +1,18 @@
+# 호텔 방배정
+
+t = int(input())
+
+for i in range(t):
+    h, w, n = map(int, input().split())
+
+    f = 0
+    ho = 0
+
+    if n % h == 0:
+        f = h * 100
+        ho = n // h
+    else:
+        f = ( n % h) * 100
+        ho = n // h + 1
+
+    print( f + ho)
