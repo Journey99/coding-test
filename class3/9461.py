62fd4f8d57616ed32053964d8d8930bcf6faac79
Journey <g48993818@gmail.com>
2021-09-07 23:41:50 +0900

    107th day

---
+++ class3/9461.py
@@ -0,0 +1,12 @@
+# 파도반 수열
+
+wh = [0 for i in range(101)]
+wh[1] = 1
+wh[2] = 1
+wh[3] = 1
+for i in range(0, 98):
+    wh[i + 3] = wh[i] + wh[i + 1]
+t = int(input())
+for i in range(t):
+    n = int(input())
+    print(wh[n])
