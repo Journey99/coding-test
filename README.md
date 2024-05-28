e1ccd8a122e750129bbfd8d7a4f43bc008c2092b
Journey99 <g48993818@gmail.com>
2024-05-28 18:15:03 +0900

    step1 clear

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Baekjoon Online Judge Studyfolio
+백준 문제풀이
+- 문제는 단계별로 진행
---
+++ step1/step1_2.py
@@ -0,0 +1,44 @@
+## 10926
+id = input()
+print(id + '??!')
+
+
+## 18108
+age = int(input())
+print(age - 543)
+
+
+## 10430
+a, b, c = map(int, input().split())
+print((a+b)%c)
+print(((a%c)+(b%c)) % c)
+print((a*b)%c)
+print(((a%c)*(b%c)) % c)
+
+
+## 2588
+a = int(input())
+b = input()
+print(a*int(b[2]))
+print(a*int(b[1]))
+print(a*int(b[0]))
+print(a*int(b))
+
+
+## 11382
+a, b, c = map(int, input().split())
+print(a+b+c)
+
+
+## 10171
+print('\\    /\\')
+print(' )  ( \')')
+print('(  /  )')
+print(' \\(__)|')
+
+## 10172
+print("|\\_/|")
+print("|q p|   /}")
+print('( 0 )"""\\')
+print('|"^"`    |')
+print("||_/=\\\\__|")
