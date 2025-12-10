60b5abaec72e0bf187bb6cee0482e5653e3f4182
Journey99 <g48993818@gmail.com>
2025-12-10 21:30:58 +0900

    leetcode - binary serach and boj

--- boj/practicecote.py
+++ boj/practicecote.py
@@ -59,9 +59,61 @@ def prblem_1244():
                 
     for i in range(0, switch_num, 20):
         print(*onoff[i:i+20])
-    
+
+from collections import Counter
+
+def problem_9017():
+    '''
+    1. 6명 다 들어왔는지부터 확인 -> n % teams == 0 이면 다 들어온거
+    2. 다 들어온 팀만 점수 확인
+    3. 점수 확인할 때 5번째 선수의 점수 따로 저장
+    '''
+
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        teams = list(map(int, input().split()))
+        
+        # 팀 인원 수 카운트
+        counter = Counter(teams)
+
+        # 6명 모두 있는 팀만 추림
+        valid = {team for team, c in counter.items() if c == 6}
+        
+        # 점수 계산 준비
+        score = {team: [] for team in valid}
+        fifth = {team : None for team in valid}
+
+        rank = 1
+
+        for team in teams:
+            if team not in valid:
+                # rank += 1
+                continue
+
+            if len(score[team]) < 4:
+                score[team].append(rank)
+            elif fifth[team] is None:
+                fifth[team] = rank
+            
+            rank += 1
+
+        
+        answer = None
+        best = (float('inf'), float('inf'))
+
+        for team in valid:
+            total = sum(score[team])
+            f = fifth[team]
+
+            if (total, f) < best:
+                best = (total, f)
+                answer = team
+
+        print(answer)
 
 
 if __name__ == "__main__":
     # problem_1205()  
-    prblem_1244()
+    # prblem_1244()
+    problem_9017()
